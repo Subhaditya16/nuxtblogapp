29412354b94d7cf6a6aec1bf918849c89dcbc60b
Subhaditya <subhaditya16@gmail.com>
2022-02-02 18:01:10 +0530

    first commit

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="post-list">
+    <PostPreview 
+      id="1"
+      thumbnail="https://image.freepik.com/free-vector/abstract-digital-technology-background-with-network-connection-lines_1017-25552.jpg?w=1380"
+      title="Hello There"
+      previewText="This is my first post" />
+    <PostPreview 
+      id="2"
+      thumbnail="https://image.freepik.com/free-vector/abstract-digital-technology-background-with-network-connection-lines_1017-25552.jpg?w=1380"
+      title="Hello There"
+      previewText="This is my second post" />
+    <PostPreview 
+      id="3"
+      thumbnail="https://image.freepik.com/free-vector/abstract-digital-technology-background-with-network-connection-lines_1017-25552.jpg?w=1380"
+      title="Hello There"
+      previewText="This is my third post" />
+  </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+
+export default {
+  components: {
+    PostPreview
+  }
+}
+</script>
+
+
+<style scoped>
+
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
+
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost" />
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm'
+
+export default {
+  layout: 'admin',
+  components: {
+    AdminPostForm
+  },
+  data() {
+    return {
+      loadedPost: {
+        author: 'Subhaditya',
+        title: 'My awesome Post',
+        content: 'Super amazing, thanks for that!',
+        thumbnailLink: 'https://image.freepik.com/free-vector/abstract-digital-technology-background-with-network-connection-lines_1017-25552.jpg?w=1380'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
